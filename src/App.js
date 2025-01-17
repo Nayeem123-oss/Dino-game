7d87dfdff42c32d5c7b746356c79ae13f4d8a44f
Nayeem123-oss <nayeemshaik741@gmail.com>
2025-01-17 18:54:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import DinoGame from './components/DinoGame';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <DinoGame />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Optional: You can add global styles here if needed
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
